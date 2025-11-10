ed2d9f7e6a07e4f181cacb53703bfeb180c6cbe6
mibahia <mariaizabel.bahia@gmail.com>
2025-11-10 16:14:23 +0000

    initial commit

---
+++ dev-init.R
@@ -0,0 +1,6 @@
+install.packages("pak")
+install.packages("pkgdepends")
+
+# Set up lock file
+pak::lockfile_create(pkg = c("devtools", "usethis", "roxygen2", "testthat", "covr", "checkmate"), lockfile = "pkg.lock", upgrade = FALSE, dependencies = NA)
+pak::lockfile_install(lockfile = "pkg.lock", update = TRUE)
