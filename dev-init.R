ddde2f51f5876083158497d233f8d90ac7e25236
mibahia <mariaizabel.bahia@gmail.com>
2025-11-13 12:05:10 +0000

    adding here

--- dev-init.R
+++ dev-init.R
@@ -2,5 +2,5 @@ install.packages("pak")
 install.packages("pkgdepends")
 
 # Set up lock file
-pak::lockfile_create(pkg = c("devtools", "usethis", "roxygen2", "testthat", "covr", "checkmate"), lockfile = "pkg.lock", upgrade = FALSE, dependencies = NA)
+pak::lockfile_create(pkg = c("devtools", "usethis", "roxygen2", "testthat", "covr", "checkmate", "here"), lockfile = "pkg.lock", upgrade = FALSE, dependencies = NA)
 pak::lockfile_install(lockfile = "pkg.lock", update = TRUE)
