cbf1ca77496e62062f77eeff5733cc50f7652559
mibahia <mariaizabel.bahia@gmail.com>
2025-11-26 11:34:41 +0000

    adding R and creating first function

---
+++ R/lds_meta_data.R
@@ -0,0 +1,16 @@
+#' @title lds_meta_data
+#' @description Fetch all available meta data
+#'
+#'
+#' @param type <character> The type of meta data to be returned "resources", "orgs", "datasets" or "topics"
+#' @example example_function()
+#' @import pkg
+#' @import pkg
+#' @importFrom checkmate assert_choice
+#' @export
+lds_meta_data <- function(type = "resources") {
+  checkmate::assert_choice(type, c("resources", "orgs", "datasets", "topics"))
+
+
+
+}
--- dev-init.R
+++ dev-init.R
@@ -1,9 +1,17 @@
 install.packages("pak")
 install.packages("pkgdepends")
 
-pak::lockfile_create(c("usethis", "here", "testthat", "checkmate", "devtools"), upgrade = FALSE, dependencies = NA, lockfile = "pkg.lock")
+pak::lockfile_create(c("usethis", "here", "testthat", "checkmate", "devtools", "glue"), upgrade = FALSE, dependencies = NA, lockfile = "pkg.lock")
 pak::lockfile_install(lockfile = "pkg.lock")
 
+# TODO: comment it out later
 usethis::use_vignette(name = "get_london_datastore_api_jey")
 usethis::use_data_raw()
 usethis::use_r("data")
+usethis::use_r("lds_meta_data")
+
+devtools::load_all() # loads all the functions in the package
+
+usethis::use_package("checkmate") # adds package to the DESCRIPTION file
+usethis::use_package("glue")
+usethis::use_air()
