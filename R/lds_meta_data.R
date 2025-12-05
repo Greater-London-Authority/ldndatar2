6b6f5ab613abafd9a87be7be9a69b3f50d6c185a
mibahia <mariaizabel.bahia@gmail.com>
2025-12-05 15:05:59 +0000

    formating dates

--- R/lds_meta_data.R
+++ R/lds_meta_data.R
@@ -4,14 +4,48 @@
 #'
 #' @param type <character> The type of meta data to be returned "resources", "orgs", "datasets" or "topics"
 #' @example example_function()
-#' @import pkg
+#' @import dplyr glue lubridate purrr readr tidyselect stringr
 #' @import pkg
 #' @importFrom checkmate assert_choice
 #' @importFrom glue glue
 #' @export
 lds_meta_data <- function(type = "resources") {
-  checkmate::assert_choice(type, c("resources", "orgs", "datasets", "topics"))
-  user_selection <- type
-  glue::glue("The user has selected: {user_selection}")
+  checkmate::assert_choice(type, c("resources", "datasets", "orgs", "topics"))
 
+  if (type %in% c("resources", "datasets")) {
+    fetch_tabular_metadata(type)
+  } else {
+    fetch_org_metadata(type)
+  }
 }
+
+#' @title fetch_tabular_metadata
+#' @noRd
+#' @description Fetch resources and datasets
+#'
+#'
+#' @param type <character>
+#' @example example_function()
+#' @import dplyr glue lubridate purrr readr tidyselect stringr
+#' @import pkg
+#' @importFrom checkmate assert_choice
+#' @importFrom glue glue
+fetch_tabular_metadata <- function(type) {
+  checkmate::assert_choice(type, c("resources", "datasets"))
+
+  meta_data_url <- glue::glue("{lds_url_api}datasets/export.{type}.csv")
+
+  # TODO do we need to clean the col names? It looks pretty clean now
+  # Maybe add checks instead???
+  # Look at JANITOR snake case
+  meta_data <- readr::read_csv(meta_data_url, show_col_types = FALSE) |>
+    dplyr::mutate(dplyr::across(
+      c(tidyselect::ends_with("At", ignore.case = FALSE)),
+      lubridate::ymd_hms
+    )) |>
+    janitor::clean_names(case = "snake")
+
+  return(meta_data)
+}
+fetch_org_metadata <- function(x) {}
+fetch_topic_metadata <- function(x) {}
