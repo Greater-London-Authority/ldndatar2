6de52dcbca6b4c517180a0217de9e7a3f856cdbe
mibahia <mariaizabel.bahia@gmail.com>
2026-05-15 11:59:57 +0100

    removing httr and adding example

--- R/lds_patch_description.R
+++ R/lds_patch_description.R
@@ -6,15 +6,14 @@
 #' @param api_key London Datastore API key - needed to amend a dataset.
 #' @return A [response()](https://httr.r-lib.org/reference/response.html) object
 #' @examples
-#' \dontrun{
-#' if(interactive()){
-#'  #EXAMPLE1
-#'  }
-#' }
+#' new_description <- lds_description_render(
+#' "datastore_description.Rmd",
+#' include_title = FALSE,
+#' save_html = FALSE)
+#' lds_patch_description(slug = "2o8xw", patch = new_description, api_key = Sys.getenv("LDS_API_KEY"))
 #' @rdname lds_patch_description
 #' @export
 #' @import checkmate
-#' @import httr
 lds_patch_description <- function(slug, patch, api_key) {
   checkmate::check_character(slug)
   checkmate::check_character(api_key)
